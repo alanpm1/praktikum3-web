7a40094d3600457e861c1b331116a3db8051738f
alanpm1 <127114378+alanpm1@users.noreply.github.com>
2023-10-24 14:29:03 +0700

    Add files via upload

---
+++ Prak3/components/Footer.jsx
@@ -0,0 +1,13 @@
+// src/components/Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; prakweb</p>
+      <p>alanalan</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Prak3/components/Hero.jsx
@@ -0,0 +1,13 @@
+// src/components/Hero.js
+import React from 'react';
+
+function Hero() {
+  return (
+    <div>
+      <h1>Websiteku</h1>
+      <p>Halooo Selamat datang</p>
+    </div>
+  );
+}
+
+export default Hero;
